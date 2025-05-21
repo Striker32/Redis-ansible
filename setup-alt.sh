284e31ddb6db94c9075135baf2d2283772b7bc3c
Striker#2 <42115372+Striker32@users.noreply.github.com>
2025-05-22 02:10:04 +0300

    Update setup-alt.sh
    Deleted generation and copying ssh keys

--- setup-alt.sh
+++ setup-alt.sh
@@ -34,21 +34,6 @@ sed -i 's/^#\?PermitEmptyPasswords .*/PermitEmptyPasswords yes/' "$SSHD_CONFIG"
 echo "[+] Перезапуск sshd..."
 systemctl restart sshd
 
-# 3. Генерация SSH ключей
-echo "[+] Генерация SSH-ключа..."
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-
-
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
-
-# 4. Копирование ключа на удалённые машины
-echo "[+] Копирование ключей на другие хосты..."
-for ip in 192.168.1.101 192.168.1.102 192.168.1.103; do
-    echo "[>] Копируем ключ на $ip"
-    ssh-copy-id -o StrictHostKeyChecking=no "root@$ip"
-done
-
 
 # 5. Включение IP forwarding
 echo "[+] Включение IP forwarding в /etc/sysctl.conf..."
